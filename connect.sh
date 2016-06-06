258ef7ea1ebfa921423203d42919fccfc45f820b
Johan M. von Behren <johan@vonbehren.eu>
2016-06-06 09:44:27 +0200

    fixes workdir of www-data user in php-fpm container

--- connect.sh
+++ connect.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-eval $(docker-machine env default)
-
 name=$(docker-compose ps | grep "php5-fpm -F" | awk '{print $1}')
 
-docker exec -it -u www-data $name bash -c 'cd /var/www/symfony; exec "${SHELL:-sh}"'
+docker exec -it -u www-data $name bash
